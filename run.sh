ce2f9f6efbc5c7332e9d666ffa922c6a1cde61bd
shawnhsueh <cttm4a1@gmail.com>
2023-04-12 16:45:17 -0700

    fourth commit

---
+++ run.sh
@@ -0,0 +1,2 @@
+export ROSETTA3=/usr/local/rosetta_src_2020.50.61505_bundle/main/source #this path should be modified to be the place where Rosetta is installed
+$ROSETTA3/bin/rosetta_scripts.default.linuxgccrelease @rosetta.flags
